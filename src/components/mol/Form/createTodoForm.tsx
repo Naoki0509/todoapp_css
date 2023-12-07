0947c5df069dee4c6f24307d9dc5f68254607381
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-12-07 16:19:30 +0900

    fea: create page styling.

--- src/components/mol/Form/createTodoForm.tsx
+++ src/components/mol/Form/createTodoForm.tsx
@@ -10,11 +10,8 @@ import styles from '~/styles/Create.module.css'
 import { TodoInputType, inputShema } from '~/types'
 
 export const TodoListForm = ({ todo }: { todo?: TodoInputType }) => {
-  const setTodoList = useSetRecoilState(todoListState)
-
   const {
     register,
-    reset,
     handleSubmit,
     formState: { errors },
   } = useForm<TodoInputType>({
@@ -30,13 +27,14 @@ export const TodoListForm = ({ todo }: { todo?: TodoInputType }) => {
     reValidateMode: 'onBlur',
   })
 
-  const addItem = (data: TodoInputType) => {
+  const setTodoList = useSetRecoilState(todoListState)
+
+  const addTodo = (data: TodoInputType) => {
     setTodoList((oldTodoList) => [...oldTodoList, data])
-    reset()
     Router.push('/')
   }
 
-  const updateItem = (data: TodoInputType) => {
+  const updateTodo = (data: TodoInputType) => {
     setTodoList((oldTodoList) => {
       const index = oldTodoList.findIndex((todo) => todo.id === data.id)
       const newTodoList = [...oldTodoList]
@@ -55,7 +53,7 @@ export const TodoListForm = ({ todo }: { todo?: TodoInputType }) => {
 
       <form
         className={styles.form}
-        onSubmit={handleSubmit(todo ? updateItem : addItem)}
+        onSubmit={handleSubmit(todo ? updateTodo : addTodo)}
       >
         <label>タイトル</label>
         <input className={styles.input} type="text" {...register('title')} />
@@ -64,7 +62,9 @@ export const TodoListForm = ({ todo }: { todo?: TodoInputType }) => {
         )}
         <label>メモ</label>
         <textarea {...register('memo')} className={styles.input} />
-        <button type="submit">{todo ? '更新' : '追加'}</button>
+        <div className={styles.buttonGroup}>
+          <button type="submit">{todo ? '更新' : '追加'}</button>
+        </div>
       </form>
     </div>
   )
